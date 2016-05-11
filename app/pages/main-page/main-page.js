80b62ddab138c9cad41fe3a7078cf7d2f79fe96c
Brightness <brightnesst99@gmail.com>
2016-05-11 21:05:38 +0200

    Completed Main Page

--- app/pages/main-page/main-page.js
+++ app/pages/main-page/main-page.js
@@ -2,10 +2,18 @@ import {Page} from 'ionic-angular';
 
 
 @Page({
-  templateUrl: 'build/pages/main-page/main-page.html'
+  	templateUrl: 'build/pages/main-page/main-page.html'
 })
 export class MainPage {
-  constructor() {
+  	constructor() {
 
-  }
+  		this.familyCardList = [
+  			{family_pic: 'img/family1-medium.png', family_title: 'Josh & Robin', location: 'New York, NY'},
+  			{family_pic: 'img/family1-medium.png', family_title: 'Josh & Robin', location: 'New York, NY'},
+  			{family_pic: 'img/family1-medium.png', family_title: 'Josh & Robin', location: 'New York, NY'},
+  			{family_pic: 'img/family1-medium.png', family_title: 'Josh & Robin', location: 'New York, NY'},
+  			{family_pic: 'img/family1-medium.png', family_title: 'Josh & Robin', location: 'New York, NY'},
+  			{family_pic: 'img/family1-medium.png', family_title: 'Josh & Robin', location: 'New York, NY'}
+  		];
+  	}
 }
